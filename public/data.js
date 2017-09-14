dcdcdffb5d90ee8bcdfe240a55fc3c3315c2a91a
crystalmilliken <crystal@millikenconsulting.com>
2017-09-14 14:55:59 -0400

    new anterior feature..exercises suggestions

--- public/data.js
+++ public/data.js
@@ -2,7 +2,8 @@ const data = [
     {
     name:"brachialis",
     description:"A muscle in the upper arm that flexes the elbow joint. It lies deeper than the biceps brachii, and makes up part of the floor of the region known as the cubital fossa. The brachialis is the prime mover of elbow flexion. While the biceps brachii appears as a large anterior bulge on the arm and commands considerable interest among body builders, the brachialis underlying it actually generates about 50% more power and is thus the prime mover of elbow flexion",
-    image:"./images/brachialis.png"
+    image:"./images/brachialis.png",
+    exercises:"Training your brachialis is a great way to help 'project' a massive arm because as the brachialis develops, it actually pushes your biceps and triceps further away from one another, making for a wider-appearing arm.</br> Best exercises are: </br>Cross-body hammer curls, Regular hammer curls, Reverse-grip barbell or EZ bar curls"
 },
  {
     name:"bicipital aponeurosis",
@@ -12,7 +13,8 @@ const data = [
 {
     name:"trapezius",
     description:"One of two large superficial muscles that extend longitudinally from the occipital bone to the lower thoracic vertebrae and laterally to the spine of the scapula (shoulder blade)",
-    image:"./images/trapezius.png"
+    image:"./images/trapezius.png",
+    exercises:"The best exercises are:</br> Barbell/dumbbell shrug variations, barbell row, dumbbell lateral raises, upright rows"
 },
 {
     name:"sternocleidomastoid",
@@ -37,17 +39,20 @@ const data = [
 {
     name:"bicep brachii",
     description:"Two-headed muscle that lies on the upper arm between the shoulder and the elbow. Both heads arise on the scapula and join to form a single muscle belly which is attached to the upper forearm.",
-    image:"./images/bicepBrachii.png"
+    image:"./images/bicepBrachii.png",
+    exercises:"Best exercises are:</br>Dumbbell/barbell curl variations, chinups, cable rows, barbell rows, inverted rows"
 },
 {
     name:"deltoid",
     description:"Is the muscle forming the rounded contour of the shoulder. Anatomically, it appears to be made up of three distinct sets of fibers though electromyography suggests that it consists of at least seven groups that can be independently coordinated by the nervous system.",
-    image:"./images/deltoid.png"
+    image:"./images/deltoid.png",
+    exercises:"The delts are comprised of 3 different muscles. Make sure to include multile exercises for a more balanced appearance. </br>Best exercises for the deltoids are:</br> Seated dumbbell shoulder press, dumbbell snatch, landmine press, plate press out with figure 8, dumbbell 'w' raises, lateral raises, mule kickbacks"
 },
 {
     name:"brachioradialis",
     description:"Flexes the forearm at the elbow. When the forearm is pronated, the brachioradialis tends to supinate as it flexes. In a supinated position, it tends to pronate as it flexes. This also assists the biceps brachii.",
-    image:"./images/brachioradialis.png"
+    image:"./images/brachioradialis.png",
+    exercises:"The best exercises are:</br>Reverse curl variations(dumbbell, barbell, cable"
 },
 {
     name:"tricep brachii",
@@ -57,27 +62,32 @@ const data = [
 {
     name:"tensor fasciae latae",
     description:"A muscle of the thigh. It is related with the gluteus maximus in function and structure and is continuous with the iliotibial tract, which attaches to the tibia. The muscle assists in keeping the balance of the pelvis while standing, walking, or running.",
-    image:"./images/tfl.png"
+    image:"./images/tfl.png",
+    exercises:"Best exercises:</br>Stair climber, standing cable raise, hip abductor machine"
 },
 {
     name:"iliopsoas",
     description:"Refers to the joined psoas and the iliacus muscles. The two muscles are separate in the abdomen, but usually merge in the thigh. As such, they are usually given the common name iliopsoas. The iliopsoas muscle joins to the femur at the lesser trochanter, and acts as the strongest flexor of the hip.",
-    image:"./images/iliopsoas.png"
+    image:"./images/iliopsoas.png",
+    exercises:"Best exercises:</br>Leg lifts(try hanging leg raises), side plank, stretches to improve tight iliopsoas muscles."
 },
 {
     name:"rectus abdominis",
     description:"Also known as the abdominals, is a paired muscle running vertically on each side of the anterior wall of the human abdomen, as well as that of some other mammals. There are two parallel muscles, separated by a midline band of connective tissue called the linea alba",
-    image:"./images/Rectus_abdominis.png"
+    image:"./images/Rectus_abdominis.png",
+    exercises:"There are many exercises that strengthen the core. Some of the best ones are:</br>Any variation of the plank, leg raises(both with or without dumbbells, stability ball exercises, variations of situps."
 },
 {
     name:"pectoralis major",
     description:"A thick, fan-shaped muscle, situated at the chest (anterior) of the human body. It makes up the bulk of the chest muscles and lies under the breast.",
-    image:"./images/pects.png"
+    image:"./images/pects.png",
+    exercises:"These exercises target both the pectoralis major and the pectoralis minor. </br>Best exercises:</br>Barbell, dumbbell, or machine decline presses, Dumbbell or machine decline flies, Weighted or machine dips, Barbell, dumbbell, or machine chest presses, Dumbbell or machine chest flies, Barbell, dumbbell, or machine incline presses, Dumbbell or machine incline flies, Pullovers or close grip pulldowns, Dumbbell front raise with arms converging at the midline of the body."
 },
 {
     name:"serratus anterior",
     description:"A muscle that originates on the surface of the 1st to 8th ribs at the side of the chest and inserts along the entire anterior length of the medial border of the scapula.",
-    image:"./images/serratus_anterior.png"
+    image:"./images/serratus_anterior.png",
+    exercises:"Best exercises are:</br>Bear crawl variations, dead-stop pushups, chest and bench press, punching"
 },
 {
     name:"flexor carpi ulnaris muscle",
@@ -92,47 +102,56 @@ const data = [
 {
     name:"flexor carpi radialis",
     description:"A muscle in the forearm that acts to flex and abduct the wrist",
-    image:"./images/Flexor-carpi-radialis.png"
+    image:"./images/Flexor-carpi-radialis.png",
+    exercises:"Best exercises:</br>Wrist curl variations"
 },
 {
     name:"abdominal external oblique",
     description:"The largest and the most superficial (outermost) of the three flat muscles of the lateral anterior abdomen.",
-    image:"./images/abdominal_external_oblique.png"
+    image:"./images/abdominal_external_oblique.png",
+    exercises:"Some of the best exercises for the obliques are:</br>Russian twist, side plank, side jacknife, decline weighted oblique crunch"
 },
 {
     name:"pectineus",
     description:"A flat, quadrangular muscle, situated at the anterior (front) part of the upper and medial (inner) aspect of the thigh. It is the most anterior adductor of the hip. The muscle does adduct and internally rotate the thigh but its primary function is hip flexion.",
-    image:"./images/pectineus.png"
+    image:"./images/pectineus.png",
+    exercises:"The pectineus muscle works with the adductors. Best exercises to strengthen these muscles are:</br>lateral lunges, side lying hip adduction, lying hip flexion, cable hip adduction, adductor machine"
 },
 {
     name:"adductor longus",
     description:"A skeletal muscle located in the thigh. One of the adductor muscles of the hip, its main function is to adduct the thigh and it is innervated by the obturator nerve. It forms the medial wall of the femoral triangle.",
-    image:"./images/adductorLongus.png"
+    image:"./images/adductorLongus.png",
+    exercises:"Best exercises to strengthen these muscles are:</br>curtsy lunges, duck walk, lateral lunges, side lying hip adduction, lying hip flexion, cable hip adduction, adductor machine"
 },
 {
     name:"sartorius",
     description:"the longest muscle in the human body. It is a long, thin, superficial muscle that runs down the length of the thigh in the anterior compartment.",
-    image:"./images/sartorius.png"
+    image:"./images/sartorius.png",
+    exercises:"Best exercises are:</br>Narrow leg squats,narrow legged leg press(careful), step ups(with or without weights)"
 },
 {
     name:"rectus femoris",
     description:"Is situated in the middle of the front of the thigh; it is fusiform in shape, and its superficial fibers are arranged in a bipenniform manner, the deep fibers running straight (rectus) down to the deep aponeurosis. Its functions are to flex the thigh at the hip joint and to extend the leg at the knee joint",
-    image:"./images/rectusFemoris.png"
+    image:"./images/rectusFemoris.png",
+    exercises:"Best exrcises are:</br>Lunge, squats, leg extensions, step ups, leg press"
 },
 {
     name:"fibularis longus",
     description:"A superficial muscle in the lateral compartment of the leg, and acts to evert and plantarflex the ankle. The muscle, the longest and most superficial of the three peroneus muscles, is attached proximally to the head of the fibula and its 'belly' runs down most of this bone.",
-    image:"./images/fibularisLongus.png"
+    image:"./images/fibularisLongus.png",
+    exercises:"Best exercises are:</br>Angled calf raises"
 },
 {
     name:"vastus lateralis",
     description:"The largest and most powerful part of the quadriceps femoris, a muscle in the thigh. Together with other muscles of the quadriceps group, it serves to extend the knee joint, moving the lower leg forward.",
-    image:"./images/vastusLateralis.png"
+    image:"./images/vastusLateralis.png",
+    exercises:"Best exercises for this muscle are:</br> Leg press, squats, lunges"
 },
 {
     name:"tibialis anterior",
     description:"The most central of all the leg muscles, and is located in the deep posterior compartment of the leg. It is the key stabilizing muscle of the lower leg",
-    image:"./images/tibialis.png"
+    image:"./images/tibialis.png",
+    exercises:"Best exercises are:</br>Toe Raises, reverse calf raises"
 },
 {
     name:"occipitofrontalis",
@@ -142,27 +161,32 @@ const data = [
 {
     name:"pectoralis minor",
     description:"A thin, triangular muscle, situated at the upper part of the chest, beneath the pectoralis major in the human body.",
-    image:"./images/pectoralisminor.png"
+    image:"./images/pectoralisminor.png", 
+    exercises:"These exercises target both the pectoralis major and the pectoralis minor. </br>Best exercises:</br>Barbell, dumbbell, or machine decline presses, Dumbbell or machine decline flies, Weighted or machine dips, Barbell, dumbbell, or machine chest presses, Dumbbell or machine chest flies, Barbell, dumbbell, or machine incline presses, Dumbbell or machine incline flies, Pullovers or close grip pulldowns, Dumbbell front raise with arms converging at the midline of the body."
 },
 {
     name:"pronator teres",
     description:"A muscle of the human body (located mainly in the forearm) that, along with the Pronator quadratus, serves to pronate the forearm (turning it so that the palm faces posteriorly when from the anatomical position).",
-    image:"./images/pronatorteres.png"
+    image:"./images/pronatorteres.png",
+    exercises:"Best exercises are:</br>Curl and Press, seated dumbbell wrist pronation"
 },
 {
     name:"gracilis",
     description:"The most superficial muscle on the medial side of the thigh. It is thin and flattened, broad above, narrow and tapering below.",
-    image:"./images/gracilis.png"
+    image:"./images/gracilis.png",
+    exercises:"Best exercises:</br>Hip adduction machine, sumo dead lift, side lunges."
 },
 {
     name:"vastus medialis",
     description:"An extensor muscle located medially in the thigh that extends the knee. It is part of the quadriceps muscle group.",
-    image:"./images/vastusmedialis.png"
+    image:"./images/vastusmedialis.png",
+    exercises:"Best exercises:</br>Bulgarian Squat, floor Extension(sit against wall, one leg bent, other leg extended. Lift extended leg and hole), leg extension machine, leg raises"
 },
 {
     name:"soleus and gastrocnemius",
     description:"Muscles of the calf. The primary function of these two muscles are to work together as plantar flexors (bending the foot back at the ankle joint) as well as flexing the leg at the knee(bending the knee) in addition to assisting propulsion and stabilisation during the actions of walking, running and jumping.",
-    image:"./images/soleus.png"
+    image:"./images/soleus.png",
+    exercises:"Best exercises:</br>Standing calf raises(lock knees to target gastrocnemius more), seated calf raises"
 }
 ]
  
